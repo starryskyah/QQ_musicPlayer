d67bdae4a7483fa5837f789f494859c74d237ab2
starryskyah <starryskyah@163.com>
2018-12-03 09:24:13 +0800

    player module

--- js/player.js
+++ js/player.js
@@ -1,13 +1,14 @@
+// Player模块
 (function(window){
     function Player(){
-        return new Player.prototype.init();
+        
     }
     Player.prototype={
         constructor:Player,
-        init:function(){
-
-        }
+        init:function(){},
     }
+
     Player.prototype.init.prototype=Player.prototype;
     window.Player=Player;
+
 })(window)
